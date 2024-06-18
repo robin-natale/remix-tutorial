b1d4f88068b5ea49d54b7451b95ea1e27f2b712f
Robin <robin.natale@swankyagency.com>
2024-06-18 15:40:32 +0100

    Added stylesheets

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,16 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+// existing imports
+
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+
 export default function App() {
   return (
     <html lang="en">
